8727d17d1bceacb3bfcc11494321444c145cffab
Masha Zorin <masha.zorin@gmail.com>
2017-01-06 12:40:18 -0500

    constructor for PolyTokens

--- PolyTokens.java
+++ PolyTokens.java
@@ -4,7 +4,19 @@ public class PolyTokens{
     
     public PolyTokens(String str){
 	this.str = str;
+	int index = 0;
 	for (int i = 0; i < str.length(); i ++){
+	    String token = "";
+	    if (Character.IsDigit(str.charAt(i))){
+		while (Character.IsDigit(str.charAt(i))){
+		    token += str.charAt(i);
+		    i ++
+		}
+		tokens[index] = token;
+	    }
+	    else if (str.charAt(i) != ' '){
+		tokens[index] = "" + str.charAt(i);
+	    }
 	}
     }
 
