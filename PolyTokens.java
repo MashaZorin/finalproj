968c45acb411df7c1d1dbcaba46d5ddd14f5e700
Masha Zorin <masha.zorin@gmail.com>
2017-01-06 14:59:12 -0500

    Methods for PolyTokens

--- PolyTokens.java
+++ PolyTokens.java
@@ -1,9 +1,11 @@
 public class PolyTokens{
     private String str;
     private String[] tokens;
+    private int currentIndex;
     
     public PolyTokens(String str){
 	this.str = str;
+	currentIndex = 0;
 	int index = 0;
 	for (int i = 0; i < str.length(); i ++){
 	    String token = "";
@@ -21,8 +23,17 @@ public class PolyTokens{
     }
 
     public boolean hasMoreTokens(){
+	if (currentIndex < tokens.length){
+	    return true;
+	}
+	else{
+	    return false;
+	}
     }
 
     public String nextToken(){
+	String out = tokens[currentIndex};
+	currentIndex ++;
+	return out;
     }
 }
